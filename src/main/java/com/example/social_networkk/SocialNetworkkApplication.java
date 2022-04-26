9c455bf3704455f7891df11d1505bb5cb7c8093b
uzytkownik <uzytkownik@kursGit.pl>
2022-04-26 11:49:05 +0200

    init

---
+++ src/main/java/com/example/social_networkk/SocialNetworkkApplication.java
@@ -0,0 +1,13 @@
+package com.example.social_networkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkkApplication.class, args);
+    }
+
+}
